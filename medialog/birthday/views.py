59c03d57200d9af47dbaff86009493d68387c41d
Espen Moe-Nilssen <espen@medialog.no>
2015-11-11 21:00:50 +0100

    cleanup

--- medialog/birthday/views.py
+++ medialog/birthday/views.py
@@ -1,22 +1,17 @@
 ## -*- coding: utf-8 -*-
 
-
 from zope.interface import implements, Interface
 from Products.Five import BrowserView
 from plone.dexterity.utils import iterSchemata
 from zope.schema import getFields
-
 from datetime import date
-
 import StringIO
 import unicodecsv as csv
-
 
 class IBirthday(Interface):
     """
     view interface
     """
-
 
 class Birthday(BrowserView):
     """
@@ -27,7 +22,6 @@ class Birthday(BrowserView):
     def date(self):
         return date.today().strftime("%d.%m.%Y")
     
-    
     @property
     def has_birthday(self):
 
@@ -37,13 +31,10 @@ class Birthday(BrowserView):
         f = StringIO.StringIO((self.context.bursdag))
         file = f.read()
         csv_reader = csv.reader(file.splitlines(), encoding='latin-1', delimiter=';' )
-        
         bursdager = []
         
         for i in csv_reader: 
             if i[4].startswith(daymonth):
-                
-                #[x.encode('utf-8') for x in i]
                 bursdager.append(i)
         
         return bursdager
